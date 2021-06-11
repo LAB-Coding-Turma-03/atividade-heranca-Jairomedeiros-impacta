6b0541347592984b62776228865d2548c6bde871
Jairomedeiros-impacta <83873161+Jairomedeiros-impacta@users.noreply.github.com>
2021-06-11 09:20:34 -0300

    Update ContaPoupanca.java

--- src/main/java/br/com/impacta/lab/ContaPoupanca.java
+++ src/main/java/br/com/impacta/lab/ContaPoupanca.java
@@ -1,9 +1,13 @@
 package br.com.impacta.lab;
 
-public class ContaPoupanca extends Conta {
+public class ContaPoupanca extends Conta{
+  
+public ContaPoupanca(int numero, int agencia, String tipo) {
+  super(numero, agencia, tipo);
+}
 
-	public ContaPoupanca(int numero, int agencia, String tipo) {
-		super(numero, agencia, tipo);
-	}
+public void alteraAgencia(int agencia) {
+    super.setAgencia(agencia);
+}
 
 }
