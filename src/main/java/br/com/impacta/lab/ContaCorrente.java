71eff080a41cfb426d5e0b5a3f5cb9770f6fca47
Jairomedeiros-impacta <83873161+Jairomedeiros-impacta@users.noreply.github.com>
2021-06-11 09:21:49 -0300

    Update ContaCorrente.java

--- src/main/java/br/com/impacta/lab/ContaCorrente.java
+++ src/main/java/br/com/impacta/lab/ContaCorrente.java
@@ -1,9 +1,10 @@
+  
 package br.com.impacta.lab;
 
-public class ContaCorrente extends Conta {
-
-	public ContaCorrente(int numero, int agencia, String tipo) {
-		super(numero, agencia, tipo);
-	}
+public class ContaCorrente extends Conta{
+  
+    public ContaCorrente(int numero, int agencia, String tipo) {
+      super(numero, agencia, tipo);
+    }
 
 }
